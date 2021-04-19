441d18bc92eb822ff08dd1ff9108a7c568f20a39
niall mcguinness <niall.mcguinness@dkit.ie>
2021-04-19 22:08:23 +0100

    Added Vector3 with Length and ToString

--- MCG_CartesianCoordinates/MCG_CartesianCoordinates.cpp
+++ MCG_CartesianCoordinates/MCG_CartesianCoordinates.cpp
@@ -2,12 +2,55 @@
 //
 
 #include <iostream>
+#include <string>
 
-/// @brief Represent a point in 3D space
-class Point3D {
+/// @brief Represent a position vector in 3D space
+class Vector3 {
+private:  //access modifier
+	float x, y, z;  //fields/member variables
+
+public: //access modifier
+
+	//constructor - method
+	Vector3(float _x, float _y, float _z) {
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+
+	float getX() {
+		return x;
+	}
+	float getY() {
+		return y;
+	}
+	float getZ() {
+		return z;
+	}
+
+	void setX(float newValue) {
+		x = newValue;
+	}
+
+	float Length() {
+		float len = sqrt(x * x + y * y + pow(z, 2));
+		return len;
+	}
+
+	std::string ToString() {
+		return "(" + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(z) + ")";
+	}
 };
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	Vector3 b(-2, 5, 3);  //instantiate a variable, b it is of type Vector3
+
+	std::cout << "Vector is " << b.ToString() << std::endl;
+	std::cout << "x value:" << b.getX() << std::endl;
+	std::cout << "length is " << b.Length() << std::endl;
+
+	b.setX(4);
+	std::cout << "new x value:" << b.getX() << std::endl;
+	std::cout << "new length is " << b.Length() << std::endl;
 }
